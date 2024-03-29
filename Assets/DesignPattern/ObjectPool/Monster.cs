24b12ad091006f7cef3514f337da158ff01899b0
flslin <84002788+flslin@users.noreply.github.com>
2024-03-29 10:08:03 +0900

    코루틴으로 생성

--- Assets/DesignPattern/ObjectPool/Monster.cs
+++ Assets/DesignPattern/ObjectPool/Monster.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 해당 작업을 위해서는 오브젝트에 Collider 컴포넌트가 연결되어 있어야함
 
-    // Update is called once per frame
-    void Update()
+    // 유니티 마우스, 터치 관련 이벤트 함수
+    // 1. OnMouseOver() : 해당 오브젝트에 마우스가 올라갈 경우 프레임마다 실행
+    // 2. OnMouseEnter() : 해당 오브젝트에 마우스가 올라갈 경우 1회 실행
+    // 3. OnMouseExit() : 해당 오브젝트에 마우스가 벗어날 때 1회 실행
+    // 4. OnMouseUp() : 해당 오브젝트에서 마우스 클릭이 해제될 때 실행
+    // 5. OnMouseDown() : 해당 오브젝트에서 마우스 클릭이 될 때 실행
+    private void OnMouseOver()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
--- Assets/DesignPattern/ObjectPool/MonsterPool.cs
+++ Assets/DesignPattern/ObjectPool/MonsterPool.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class MonsterPool : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // [무한 생성기의 경우]
+    // 1. Update 문으로 구현
+    // 2. Coroutine을 이용한 설계
+
+    public GameObject monsterPrefab; // 몬스터 프리팹에 대한 값
+    public int poolSize; // 풀로 생성할 유닛의 개수
+    public float spawnTime; // 몬스터의 젠(생성) 시간
+    GameObject[] monsterPool; // 몬스터에 대한 배열 (몬스터 풀)
+
     void Start()
     {
-        
+        monsterPool = new GameObject[poolSize]; // 풀에서 설정된 사이즈만큼 할당
+
+        Spawn(monsterPool, poolSize); // 할당한 배열만큼 생성 진행
+
+        StartCoroutine("MonsterPooling"); // 특정시간마다 몬스터가 생성될 수 있도록 코루틴 작동
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spawn(GameObject[] monsterPool, int poolSize)
     {
-        
+        for (int i = 0; i < poolSize; i++)
+        {
+            monsterPool[i] = Instantiate(monsterPrefab); // 몬스터 프리팹 생성
+            monsterPool[i].name = "Monster_" + i; // 생성 몬스터 이름 지정
+            
+            monsterPool[i].SetActive(false); // 비활성화 처리
+        }
+    }
+
+    IEnumerator MonsterPooling()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+
+            for(int i = 0; i<poolSize; i++)
+            {
+                if (monsterPool[i].activeSelf == true)
+                    continue;
+
+                float x = Random.Range(-5, 5);
+                float y = Random.Range(-5, 5); // 3D로 생성을 하는 경우 x, z축으로 설계
+
+                monsterPool[i].transform.position = new Vector2(x, y); // 3D일 경우 Vector3
+
+                monsterPool[i].SetActive(true); // 활성화
+                break;
+            }
+        }
     }
 }
